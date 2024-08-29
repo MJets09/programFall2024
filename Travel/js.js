741fb02be2863e6c4750402d721e7148abdc736f
MJets09 <mthaoqk@gmail.com>
2024-08-28 21:23:14 -0500

    8.28.24

---
+++ Travel/js.js
@@ -0,0 +1,7 @@
+document.getElementById("doi").addEventListener("click", removeElement)
+document.getElementById("kohChang").addEventListener("click", removeElement)
+
+function removeElement() {
+        this.remove();
+    }
+
