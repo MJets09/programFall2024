3afa33bd9ba61d8a05d9a661a8ba34aa9335eb6c
MJets09 <mthaoqk@gmail.com>
2024-09-10 21:30:23 -0500

    9.10.24

--- practices/domJs.js
+++ practices/domJs.js
@@ -5,9 +5,52 @@ document.getElementById('changeText').addEventListener('click', function() {
 })
 
 document.getElementById('toggleStyle').addEventListener('click', function() {
-    document.getElementById('toggleStyle').classList.add('highlight')
+    document.getElementById('toggleStyle').classList.add('base')
+    document.getElementById('toggleStyle').classList.remove('highlight')
 })
 
 document.getElementById('hideElement').addEventListener('click', function(){
     document.getElementById('hideElement').style.display = 'none';
 })
+
+document.getElementById("toggleStyle").setAttribute('class', 'highlight')
+
+
+const liList = document.getElementsByTagName('li')
+
+console.log(liList[3].textContent)
+
+const listColor = document.getElementsByTagName('li');
+
+console.log(listColor)
+
+document.getElementById('changeBlue').addEventListener('click', function() {
+
+    for(i = 0; i < listColor.length; i++) {
+
+        listColor[i].classList.add('highlight')
+        listColor[i].classList.remove('base')
+    }
+
+})
+
+document.getElementById('changeRed').addEventListener('click', function() {
+
+    for(i = 0; i < listColor.length; i++) {
+        listColor[i].classList.add('base')
+        listColor[i].classList.remove('highlight')
+    }
+
+})
+
+
+document.getElementById('add').addEventListener('click', function(){
+    createLi = document.createElement('li')
+    textNode = document.createTextNode("Water")
+
+    createLi.appendChild(textNode);
+
+    document.getElementById('sList').appendChild(createLi)
+})
+
+console.log(document.getElementById('stars').lastElementChild)
